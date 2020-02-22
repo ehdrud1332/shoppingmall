2c0c18e0d7b7537f80bbbe2ec68b3bab302ba731
bob <ehdrud1332@gmail.com>
2020-02-22 13:26:41 +0900

    order를 product파일과 연계

--- routes/orders.js
+++ routes/orders.js
@@ -1,18 +1,64 @@
 const express = require('express');
 const router = express.Router();
-
+const orderModel = require("../models/order");
+const productModel = require("../models/product");
 // 장바구니를 불러오는 API
 router.get('/', (req, res) => {
-    res.json({
-        msg : "장바구니를 불러옴"
-    });
+   orderModel
+        .find()
+        .exec()
+        .then(docs => {
+            res.json({
+                count: docs.length,
+                orderlist: docs
+            });
+        })
+        .catch(err => {
+            res.json({
+                error: err
+            });
+        });
 });
 
 // 장바구니를 생성하는 API
 router.post('/', (req, res) => {
-    res.json({
-        msg : "장바구니를 생성함"
-    });
+    
+    productModel
+        .findById(req.body.product)
+        .then(product => {
+            if(!product){
+                return res.json({
+                    msg : "product not found"
+                });
+            } else {
+                const order =  new orderModel({
+                    product: req.body.product,
+                    qty: req.body.qty
+                
+                });
+                return order.save();
+            }
+        })
+        .then(result => {
+            res.json({
+                msg : "created order",
+                createdOrder: {
+                    product: result.product,
+                    qty: result.qty,
+                    id: result._id,
+                    request: {
+                        type: "GET",
+                        url: "http://localhost:2020/order/"
+                    }
+                }
+            });
+
+        })
+        .catch(err => {
+            res.json({
+                error : err
+            })
+        });
 });
 
 // 장바구니를 수정하는 API
