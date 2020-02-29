0394d57a344c07a6d148bc14671706f00f1f1b99
bob <ehdrud1332@gmail.com>
2020-02-29 10:14:04 +0900

    log, sign up API

--- routes/user.js
+++ routes/user.js
@@ -1,28 +1,41 @@
 const express = require('express');
 const router = express.Router();
+const userModel = require('../models/user');
 
-// 유저를 불러오는 API
+// 유저를 불러오는 API(관리자용)
 router.get('/', (req, res) => {
     res.json({
         msg : "유저를 불러옴"
     });
 });
 
-//유저를 생성하는 API
-router.post('/', (req, res) => {
+
+// 회원가입
+// http://localhost:2020/user/signup
+router.post('/signup', (req, res) => {
     res.json({
         msg : "유저를 생성함"
     });
 });
 
-//유저를 수정하는 API
+// 로그인
+// http://localhost:2020/uesr/login
+router.post('/login', (req, res) => {
+    
+});
+
+
+
+
+
+//유저 프로필 업데이트
 router.patch('/', (req, res) => {
     res.json({
         msg : "유저를 수정함"
     });
 });
 
-//유저를 삭제하는 API
+//회원탈퇴
 router.delete('/', (req, res) => {
     res.json({
         msg : "유저를 삭제함"
