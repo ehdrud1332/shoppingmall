02c217d5df9ac8ae774edf07c4b23be682b0aed8
bob <ehdrud1332@gmail.com>
2020-02-29 10:43:01 +0900

    회원가입 API절차

--- routes/user.js
+++ routes/user.js
@@ -17,39 +17,51 @@ router.get('/', (req, res) => {
 // http://localhost:2020/user/signup
 router.post('/signup', (req, res) => {
 
-    // password 암호화
-    bcrypt.hash(req.body.password, 10, (err, hash) => {
-        
-        if(err) {
-            // 패스워드 암호화 실패시 나타남
-            return res.json({
+    userModel
+        .findOne({email: req.body.email})
+        .then(user => {
+            if(user) {
+                return res.json({
+                    msg: "mail exists"
+                });
+            } else {
+                // password 암호화
+                bcrypt.hash(req.body.password, 10, (err, hash) => {
+                        
+                    if(err) {
+                        // 패스워드 암호화 실패시 나타남
+                        return res.json({
+                            error: err
+                        });
+                    } else {
+                        // 패스워드 암호와 성공시 처리 내용
+                        const user = new userModel({
+                            username: req.body.username,
+                            email: req.body.email,
+                            password: hash
+                        });
+                        user
+                            .save()
+                            .then(user => {
+                                res.json({
+                                    msg: "resigisted user",
+                                    userInfo: user
+                                })
+                            })
+                            .catch(err => {
+                                res.json({
+                                    error : err
+                                });
+                            });
+                    }
+                })
+            }
+        })
+        .catch(err => {
+            res.json({
                 error: err
             });
-        } else {
-            // 패스워드 암호와 성공시 처리 내용
-            const user = new userModel({
-                username: req.body.username,
-                email: req.body.email,
-                password: hash
-            });
-            user
-                .save()
-                .then(user => {
-                    res.json({
-                        msg: "resigisted user",
-                        userInfo: user
-                    })
-                })
-                .catch(err => {
-                    res.json({
-                        error : err
-                    });
-                });
-        }
-    })
-
-
-
+        })
 
 });
 
