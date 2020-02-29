40d8627120262f63fa0f7bdf5cc6333995103668
bob <ehdrud1332@gmail.com>
2020-02-29 10:53:43 +0900

    add email regix

--- models/user.js
+++ models/user.js
@@ -7,7 +7,10 @@ const userSchema = mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true,
+        match: /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/
+  
     },
     password: {
         type: String,
